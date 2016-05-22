c077a100a925914c463782304abc42819163a8f1
nicolasvdl <nicolas.vandelook@student.odisee.be>
2016-05-22 14:57:23 +0200

    Add files via upload

---
+++ project M&I/hintsplaatsen.js
@@ -0,0 +1,198 @@
+function hintsplaatsen(id){
+if(id == '1' || id == '1b' || id == '4'|| id == '4b'|| id == '12'|| id == '12b')
+                { 
+                    
+                    var R = Math.floor((Math.random() * 6) + 1);
+                    
+                    switch (R) {
+                    case 1:
+                        var hint1 = '1.3.png';
+                        var hint2 = '2.4.png';
+                        var hint3 = '2.1.png';
+                            
+                        break;
+                    case 2:
+                        var hint1 = '1.3.png';
+                        var hint2 = '2.1.png';
+                        var hint3 = '2.4.png';
+                        
+                        break;
+                    case 3:
+                        var hint1 = '1.4.png';
+                        var hint2 = '2.3.png';
+                        var hint3 = '2.1.png';
+                            
+                        break;
+                    case 4:
+                        var hint1 = '1.4.png';
+                        var hint2 = '2.1.png';
+                        var hint3 = '2.3.png';
+                        
+                        break;
+                    case 5:
+                        var hint1 = '1.1.png';
+                        var hint2 = '2.3.png';
+                        var hint3 = '2.4.png';
+                        
+                        break;
+                    case 6:
+                        var hint1 = '1.1.png';
+                        var hint2 = '2.4.png';
+                        var hint3 = '2.3.png';
+                        
+                        break;
+                    default:
+                        
+                    }
+                    $('#hint1'+ id).prepend('<img class="nodragimg hint" src="images/hint1/'+hint1+'" />');
+                    $('#hint2'+ id).prepend('<img class="nodragimg hint" src="images/hint2/'+hint2+'" />');
+                    $('#hint3'+ id).prepend('<img class="nodragimg hint" src="images/hint3/'+hint3+'" />');
+                }
+                 if(id == '2' || id == '2b' || id == '3'|| id == '3b'|| id == '10'|| id == '10b'|| id == '11'|| id == '11b')
+                { 
+                    
+                    var R = Math.floor((Math.random() * 6) + 1);
+                   
+                    switch (R) {
+                    case 1:
+                        var hint1 = '1.3.png';
+                        var hint2 = '2.2.png';
+                        var hint3 = '2.1.png';
+                            
+                        break;
+                    case 2:
+                        var hint1 = '1.3.png';
+                        var hint2 = '2.1.png';
+                        var hint3 = '2.2.png';
+                        
+                        break;
+                    case 3:
+                        var hint1 = '1.2.png';
+                        var hint2 = '2.3.png';
+                        var hint3 = '2.1.png';
+                            
+                        break;
+                    case 4:
+                        var hint1 = '1.2.png';
+                        var hint2 = '2.1.png';
+                        var hint3 = '2.3.png';
+                        
+                        break;
+                    case 5:
+                        var hint1 = '1.1.png';
+                        var hint2 = '2.3.png';
+                        var hint3 = '2.2.png';
+                        
+                        break;
+                    case 6:
+                        var hint1 = '1.1.png';
+                        var hint2 = '2.2.png';
+                        var hint3 = '2.3.png';
+                        
+                        break;
+                    default:
+                        
+                    }
+                    $('#hint1'+ id).prepend('<img class="nodragimg hint" src="images/hint1/'+hint1+'" />');
+                    $('#hint2'+ id).prepend('<img class="nodragimg hint" src="images/hint2/'+hint2+'" />');
+                    $('#hint3'+ id).prepend('<img class="nodragimg hint" src="images/hint3/'+hint3+'" />');
+                }
+                 if(id == '5' || id == '5b' || id == '9'|| id == '9b'|| id == '14'|| id == '14b'|| id == '15'|| id == '15b')
+                { 
+                    
+                    var R = Math.floor((Math.random() * 6) + 1);
+                    
+                    switch (R) {
+                    case 1:
+                        var hint1 = '1.4.png';
+                        var hint2 = '2.2.png';
+                        var hint3 = '2.1.png';
+                            
+                        break;
+                    case 2:
+                        var hint1 = '1.4.png';
+                        var hint2 = '2.1.png';
+                        var hint3 = '2.2.png';
+                        
+                        break;
+                    case 3:
+                        var hint1 = '1.2.png';
+                        var hint2 = '2.4.png';
+                        var hint3 = '2.1.png';
+                            
+                        break;
+                    case 4:
+                        var hint1 = '1.2.png';
+                        var hint2 = '2.1.png';
+                        var hint3 = '2.4.png';
+                        
+                        break;
+                    case 5:
+                        var hint1 = '1.1.png';
+                        var hint2 = '2.4.png';
+                        var hint3 = '2.2.png';
+                        
+                        break;
+                    case 6:
+                        var hint1 = '1.1.png';
+                        var hint2 = '2.2.png';
+                        var hint3 = '2.4.png';
+                        
+                        break;
+                    default:
+                        
+                    }
+                    $('#hint1'+ id).prepend('<img class="nodragimg hint" src="images/hint1/'+hint1+'" />');
+                    $('#hint2'+ id).prepend('<img class="nodragimg hint" src="images/hint2/'+hint2+'" />');
+                    $('#hint3'+ id).prepend('<img class="nodragimg hint" src="images/hint3/'+hint3+'" />');
+                }
+                if(id == '6' || id == '6b' || id == '7'|| id == '7b'|| id == '8'|| id == '8b')
+                { 
+                    
+                    var R = Math.floor((Math.random() * 6) + 1);
+                 
+                    switch (R) {
+                    case 1:
+                        var hint1 = '1.4.png';
+                        var hint2 = '2.2.png';
+                        var hint3 = '2.3.png';
+                            
+                        break;
+                    case 2:
+                        var hint1 = '1.4.png';
+                        var hint2 = '2.3.png';
+                        var hint3 = '2.2.png';
+                        
+                        break;
+                    case 3:
+                        var hint1 = '1.2.png';
+                        var hint2 = '2.4.png';
+                        var hint3 = '2.3.png';
+                            
+                        break;
+                    case 4:
+                        var hint1 = '1.2.png';
+                        var hint2 = '2.3.png';
+                        var hint3 = '2.4.png';
+                            
+                        break;
+                    case 5:
+                        var hint1 = '1.3.png';
+                        var hint2 = '2.4.png';
+                        var hint3 = '2.2.png';
+                        
+                        break;
+                    case 6:
+                        var hint1 = '1.3.png';
+                        var hint2 = '2.2.png';
+                        var hint3 = '2.4.png';
+                        
+                        break;
+                    default:
+                        
+                    }
+                    $('#hint1'+ id).prepend('<img class="nodragimg hint" src="images/hint1/'+hint1+'" />');
+                    $('#hint2'+ id).prepend('<img class="nodragimg hint" src="images/hint2/'+hint2+'" />');
+                    $('#hint3'+ id).prepend('<img class="nodragimg hint" src="images/hint3/'+hint3+'" />');
+                }
+}
